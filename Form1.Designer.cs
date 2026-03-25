8a818797557152a71b4bebe2aeaff6d7a7fdffdf
Parker Arnett <362581936@GoTVDSB.ca>
2026-03-25 12:28:55 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Programming_Assignments_Topic_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnGood = new System.Windows.Forms.Button();
+            this.btnBad = new System.Windows.Forms.Button();
+            this.imgGood = new System.Windows.Forms.PictureBox();
+            this.imgBad = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgGood)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGood
+            // 
+            this.btnGood.Location = new System.Drawing.Point(81, 37);
+            this.btnGood.Name = "btnGood";
+            this.btnGood.Size = new System.Drawing.Size(167, 104);
+            this.btnGood.TabIndex = 0;
+            this.btnGood.Text = "Good";
+            this.btnGood.UseVisualStyleBackColor = true;
+            this.btnGood.Click += new System.EventHandler(this.btnGood_Click);
+            // 
+            // btnBad
+            // 
+            this.btnBad.Location = new System.Drawing.Point(372, 37);
+            this.btnBad.Name = "btnBad";
+            this.btnBad.Size = new System.Drawing.Size(167, 104);
+            this.btnBad.TabIndex = 1;
+            this.btnBad.Text = "Bad";
+            this.btnBad.UseVisualStyleBackColor = true;
+            this.btnBad.Click += new System.EventHandler(this.btnBad_Click);
+            // 
+            // imgGood
+            // 
+            this.imgGood.Image = ((System.Drawing.Image)(resources.GetObject("imgGood.Image")));
+            this.imgGood.Location = new System.Drawing.Point(200, 191);
+            this.imgGood.Name = "imgGood";
+            this.imgGood.Size = new System.Drawing.Size(243, 247);
+            this.imgGood.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgGood.TabIndex = 2;
+            this.imgGood.TabStop = false;
+            this.imgGood.Visible = false;
+            // 
+            // imgBad
+            // 
+            this.imgBad.Image = global::Programming_Assignments_Topic_9.Properties.Resources.prisoner;
+            this.imgBad.Location = new System.Drawing.Point(200, 191);
+            this.imgBad.Name = "imgBad";
+            this.imgBad.Size = new System.Drawing.Size(243, 247);
+            this.imgBad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgBad.TabIndex = 3;
+            this.imgBad.TabStop = false;
+            this.imgBad.Visible = false;
+            this.imgBad.Click += new System.EventHandler(this.imgBad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 450);
+            this.Controls.Add(this.imgBad);
+            this.Controls.Add(this.imgGood);
+            this.Controls.Add(this.btnBad);
+            this.Controls.Add(this.btnGood);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgGood)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBad)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGood;
+        private System.Windows.Forms.Button btnBad;
+        private System.Windows.Forms.PictureBox imgGood;
+        private System.Windows.Forms.PictureBox imgBad;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programming_Assignments_Topic_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGood_Click(object sender, EventArgs e)
+        {
+            imgGood.Visible = true;
+        }
+
+        private void btnBad_Click(object sender, EventArgs e)
+        {
+            imgBad.Visible = true;
+        }
+
+        private void imgBad_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
